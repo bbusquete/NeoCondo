7ae1d2d2aa10ef8d49e7874533c73a53517f2c90
Bruno Busquete <103598746+bbusquete@users.noreply.github.com>
2023-11-17 23:14:22 -0300

    Ajuste e melhorias de logs
    Ajuste e melhorias de logs

--- src/NeoCondo.GerarBoleto/Program.cs
+++ src/NeoCondo.GerarBoleto/Program.cs
@@ -1,14 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NeoCondo.GerarBoleto;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Iniciando gerações de cobranças...");
 
 var g = new GerarBoletoService();
 var cobrancas = g.LerArquivoCobrancas();
+Console.WriteLine("Será gerado um total de {0} cobranças", cobrancas.Count);
 
 foreach (var c in cobrancas)
 {
+    Console.WriteLine("Gerando cobrança da " + c.ExternalReference);
     var ret = await g.CreateCobrancaAssync(c);
 }
 
-Console.WriteLine("Bye, World!");
+Console.WriteLine("Processo de Cobrança Finalizado");
